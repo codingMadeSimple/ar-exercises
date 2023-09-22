6ab8d406ee81afc6ba68ecf9c087cdd20e712c31
Colin Spangelo <colin.spangelo@gmail.com>
2023-09-22 13:21:42 -0600

    Added then deleted an instance of the third store in the database.

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,10 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+#save instance variable for the third store
+@store3 = Store.find(3)
+#delete the instance
+@store3.delete
+
+puts "There are #{Store.count} stores."
